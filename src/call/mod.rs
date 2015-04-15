9d2d45db723017e0419eaf152b3c43b35d9b9207
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-04-14 20:00:35 -0400

    Refactoring.

--- src/call/mod.rs
+++ src/call/mod.rs
@@ -24,19 +24,21 @@ pub fn call<P: AsRef<Path>, C: Caller>(path: &P, query: C, fdr: Prob, threads: u
     let mut records = reader.records();
 
     let mut pool = simple_parallel::Pool::new(threads);
-    let call = |likelihoods: &Vec<GenotypeLikelihoods>| query.call(likelihoods);
+    let call = |likelihoods: Vec<GenotypeLikelihoods>| query.call(&likelihoods);
 
-    let mut buffer = Vec::with_capacity(1000);
+    let mut site_buffer = Vec::with_capacity(1000);
+    let mut prob_buffer = Vec::with_capacity(1000);
     let mut candidates = Vec::with_capacity(1000);
 
     loop {
-        buffer.extend(records.by_ref().take(1000).map(|record| Site::new(record.ok().expect("Error reading BCF."))));
-        let likelihoods: Vec<Vec<GenotypeLikelihoods>> = buffer.iter_mut().map(|site| site.genotype_likelihoods()
-                                                       .ok()
-                                                       .expect("Error reading genotype likelihoods.")
-        ).collect();
+        site_buffer.extend(records.by_ref().take(1000).map(|record| Site::new(record.ok().expect("Error reading BCF."))));
+        prob_buffer.extend(pool.map(
+            site_buffer.iter_mut().map(
+                |site| site.genotype_likelihoods().ok().expect("Error reading genotype likelihoods.")
+            ), &call)
+        );
 
-        candidates.extend(buffer.drain().zip(pool.map(likelihoods.iter(), &call)).filter(|&(_, prob)| prob < fdr));
+        candidates.extend(site_buffer.drain().zip(prob_buffer.drain()).filter(|&(_, prob)| prob < fdr));
     }
     // TODO go on with candidates (control FDR, write)
 }
