c0503ee9f1b370e1cd808b972b07b918882db22a
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-04-19 20:36:46 -0400

    Fixes.

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -14,7 +14,7 @@ peg_file! parser("grammar.rustpeg");
 
 
 pub fn parse(query: &str, sample_idx: &HashMap<String, usize>, heterozygosity: Prob) -> Box<call::Caller> {
-    let expr = parser::expr(query).ok().expect("Error parsing query.");
+    let expr = parser::expr(query).unwrap();
     expr.caller(sample_idx, heterozygosity)
 }
 
@@ -26,4 +26,24 @@ pub fn sample_index(bcf: &bcf::Reader) -> HashMap<String, usize> {
     }
 
     sample_idx
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_parse() {
+        let mut sample_idx = HashMap::new();
+        sample_idx.insert("A.0".to_string(), 0);
+        sample_idx.insert("B_0".to_string(), 0);
+        sample_idx.insert("C0".to_string(), 0);
+
+        parse("A.0 - B_0", &sample_idx, 0.001);
+        parse("A.0", &sample_idx, 0.001);
+        parse("A.0 - (B_0 + C0)", &sample_idx, 0.001);
+        parse("A.0 x B_0 x C0 with k = 2", &sample_idx, 0.001);
+    }
 }
